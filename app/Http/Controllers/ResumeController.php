f08a71eacae2172906e76393b7d9dbe7aab6af1e
zak <zspratt21@gmail.com>
2023-05-18 11:30:56 +0800

    Created stub for resume controller

---
+++ app/Http/Controllers/ResumeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class ResumeController extends Controller
+{
+    public function show(string $id): View
+    {
+
+        return view();
+    }
+
+}
