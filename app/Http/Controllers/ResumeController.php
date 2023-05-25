a8bfb5a708c1412320ecced4fc1f159814c4d8fb
Zak <zspratt21@gmail.com>
2023-05-25 23:01:48 +0800

    TODOs for preparing data in resume controller.

--- app/Http/Controllers/ResumeController.php
+++ app/Http/Controllers/ResumeController.php
@@ -12,6 +12,16 @@ class ResumeController extends Controller
         $config = [
             'document'  => view('welcome')->render(),
         ];
+        // @todo user - phone
+        // @todo user - profile_photo
+        // @todo user - introduction
+        // @todo user - name
+        // @todo user - address
+        // @todo skills
+        //   query skill links and grab url and logo of linked skills
+        // @todo experiences
+        //   split array by type, grab name and image of linked entity, and title, description and dates of experience
+        //   include milestones via query on experience_id under experience item(title, image and description)
         $result = $pdfreactor->convertAsBinary($config);
         header("Content-Type: application/pdf");
         echo $result;
