118d191a05d13b139206faaadf9ca36fd35f752c
Zak <zspratt21@gmail.com>
2023-05-30 00:00:59 +0800

    modified guest to utilize same navigation as app layout.

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -16,7 +16,47 @@
     </head>
     <body class="antialiased">
         <div class="font-sans text-gray-900 dark:text-gray-100 antialiased">
-            {{ $slot }}
+            <nav x-data="{ open: false }" class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
+                <!-- Primary Navigation Menu -->
+                <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                    <div class="flex justify-between h-16">
+                        <div class="flex">
+                            <!-- Logo -->
+                            <div class="shrink-0 flex items-center">
+                                <a href="{{ url('/') }}">
+                                    <x-laravel-logo class="block h-9 w-auto" />
+                                </a>
+                            </div>
+
+                            <!-- Navigation Links -->
+                            <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                                <x-nav-link href="{{ route('login') }}" :active="request()->routeIs('login')">
+                                    {{ __('Login') }}
+                                </x-nav-link>
+                            </div>
+                            <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                                <x-nav-link href="{{ route('register') }}" :active="request()->routeIs('register')">
+                                    {{ __('Register') }}
+                                </x-nav-link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+
+            <!-- Page Heading -->
+            @if (isset($header))
+                <header class="bg-white dark:bg-gray-800 shadow">
+                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                        {{ $header }}
+                    </div>
+                </header>
+            @endif
+
+            <!-- Page Content -->
+            <main>
+                {{ $slot }}
+            </main>
         </div>
     </body>
 </html>
